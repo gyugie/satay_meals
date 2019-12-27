a3a6704a7b6b95cc7a7a14375406136cd7c9efa8
mugi <mugypleci@gmail.com>
2019-12-27 11:05:46 +0700

    setup google maps

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -7,6 +7,7 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+  [GMSServices provideAPIKey: @AIzaSyB-ed7fvN577q8h7s7srJVqoTKO_srddAo];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
